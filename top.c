32b3ed018d5cde2257af92019dac469c10aebaf8
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-14 13:57:40 +0300

    Update top.c

--- top.c
+++ top.c
@@ -1,91 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include "top.h"
-
 int _strlen(char *s)
 {
-    int cou = 0;
-    while (*s != '\0')
-    {
-        s++;
-        cou++;
-    }
-    return (cou);
-}
-
-int main(void)
+int cou = 0;
+while (*s != '\0')
 {
-    char *input = NULL;
-    ssize_t checkline;
-    size_t s = 0;
-    char *input_cp = NULL;
-    char **argv;
-    char *tok;
-    int tok_counter = 0;
-    int u = 0;
-    const char *delim = " \t\n";
-
-    while (1)
-    {
-        print_top("top$");
-        checkline = getline(&input, &s, stdin);
-        if (checkline == -1)
-        {
-            perror("Error reading input:");
-            free(input);
-            free(input_cp);
-            free(argv);
-            return (-1);
-        }
-        else if (checkline == 1 && input[0] == '\n')
-        {
-           
-            continue;
-        }
-
-        input_cp = input;
-        tok = strtok(input, delim);
-        while (tok != NULL)
-        {
-            tok_counter++;
-            tok = strtok(NULL, delim);
-        }
-        tok_counter++;
-        argv = malloc(sizeof(char *) * tok_counter);
-        tok = strtok(input_cp, delim);
-        for (u = 0; tok != NULL; u++)
-        {
-            argv[u] = malloc(sizeof(char) * strlen(tok));
-            strcpy(argv[u], tok);
-            tok = strtok(NULL, delim);
-        }
-        argv[u] = NULL;
-
-        if (access(argv[0], F_OK) != 0)
-        {
-            print_top("Command does not exist.\n");
-        }
-        else if (strcmp(input, "exit\n") == 0)
-        {
-            free(input);
-            free(input_cp);
-            free(argv);
-            return (0);
-        }
-        else
-        {
-            if (topcmd(argv) == -1)
-            {
-                (void) topcmd(argv);
-                print_top("Error executing command.\n");
-            }
-        }
-
-        free(argv);
-    }
-
-    return 0;
+s++;
+cou++;
+}
+return (cou);
+}
+int main(int ac, char **argv)
+{
+  char *input = NULL;
+  ssize_t checkline;
+  size_t s = 0;
+  (void) ac;
+  (void) argv;
+  while (1)
+  {
+print_top("top$");
+checkline = getline(&input, &s, stdin);
+if(checkline == -1)
+  {  free(input);
+   return (-1); 
 }
 
+}
+free(input); 
+  return (0);
+}
