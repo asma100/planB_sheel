5af84cb949d5dc6c7b3783fb4b00fa15f3a9e3fd
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-14 14:43:19 +0300

    Update topexec.c

--- topexec.c
+++ topexec.c
@@ -4,7 +4,7 @@ void topcmd(char **argv){
     char *cmd = NULL, *acmd = NULL;
 
     pid_t pid ;
-   if (argv){
+    if (argv){
         /* get the command */
         cmd = argv[0];
 
@@ -12,19 +12,29 @@ void topcmd(char **argv){
         acmd = finding_path(cmd);
 
         /* create a child process to execute the command */
-         pid = fork();
+        pid = fork();
         if (pid == -1){
             perror("Error:");
-              exit(EXIT_FAILURE);
+            exit(EXIT_FAILURE);
         }
-       else if (pid == 0) {
-        /* Child process*/
-       execve(acmd, argv, NULL);
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    } else {
-        /* Parent process*/
-        wait(NULL);
+        else if (pid == 0) {
+            /* Child process*/
+            if (execve(acmd, argv, NULL) == -1) {
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            /* Parent process*/
+            int status;
+            if (wait(&status) == -1) {
+                perror("wait");
+                exit(EXIT_FAILURE);
+            }
+            if (WIFEXITED(status)) {
+                printf("Child process exited with status %d\n", WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                printf("Child process terminated by signal %d\n", WTERMSIG(status));
+            }
+        }
     }
-}
 }
