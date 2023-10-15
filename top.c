6c360e57b6be28505bfc814613d24b80a2433087
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-15 18:09:35 +0300

    Update top.c

--- top.c
+++ top.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "top.h"
-
 int _strlen(char *s) {
   int cou = 0;
   while (*s != '\0') {
@@ -11,10 +10,8 @@ int _strlen(char *s) {
   }
   return (cou);
 }
-
 int main(void) {
  
-
   char *input = NULL;
   ssize_t checkline;
   size_t s = 0;
@@ -24,7 +21,6 @@ int main(void) {
   char **argv = NULL;
   int u = 0;
   int j;
-  int i;
   const char *delim = " \t\n";
 if (isatty (STDIN_FILENO)) {
 /* shell is running in interactive mode */
@@ -52,7 +48,6 @@ if (isatty (STDIN_FILENO)) {
     free(argv); 
     return (0);
   }
-
     input_cp = malloc(sizeof(char) * (checkline + 1));
     if (input_cp == NULL) {
       perror("Error allocating memory:");
@@ -60,22 +55,18 @@ if (isatty (STDIN_FILENO)) {
       free(argv);
       return (-1);
     }
-    else {
-  free(input_cp);
-}
     strcpy(input_cp, input);
-
     tok = strtok(input, delim);
     while (tok != NULL) {
       tok_counter++;
       tok = strtok(NULL, delim);
     }
     tok_counter++;
-
     argv = malloc(sizeof(char *) * tok_counter);
     if (argv == NULL) {
       perror("Error allocating memory for argv:");
       free(input);
+      free(input_cp);
       free(input_cp);
       return (-1);
     }
@@ -97,21 +88,16 @@ if (isatty (STDIN_FILENO)) {
       tok = strtok(NULL, delim);
     }
     argv[u] = NULL;
-
     topcmd(argv);
   }
-
   free(input);
   free(input_cp);
 } else {
 /* shell is running in non-interactive mode */
 /* execute commands from script or batch file*/
-
-
     char *line = NULL;
     size_t len = 0;
     ssize_t checkline;
-
     while ((checkline = getline(&line, &len, stdin)) != -1) {
         /*read each line from standard input*/
         if (checkline == 1 && line[0] == '\n') {
@@ -124,25 +110,18 @@ if (isatty (STDIN_FILENO)) {
             int tok_counter = 0;
             char *tok;
             const char *delim = " \n";
-
             tok = strtok(line, delim);
             while (tok != NULL) {
                 tok_counter++;
                 tok = strtok(NULL, delim);
             }
             tok_counter++;
-
             argv = malloc(sizeof(char *) * tok_counter);
             if (argv == NULL) {
                 perror("Error allocating memory for argv:");
                 free(line);
                 return (-1);
-              for ( i = 0; i < u; i++) {
-    free(argv[i]);
-}
-free(argv);
             }
-
             tok = strtok(line, delim);
           
             for (u = 0; tok != NULL; u++) {
@@ -160,9 +139,7 @@ free(argv);
                 tok = strtok(NULL, delim);
             }
             argv[u] = NULL;
-
             topcmd(argv);
-
             /* free memory allocated for argv*/
             for ( j = 0; j < u; j++) {
                 free(argv[j]);
@@ -170,11 +147,8 @@ free(argv);
             free(argv);
         }
     }
-
     /* free memory*/
     free(line);
 }
-
-free(argv);
   return (0);
 }
