73d156b5d55be359ee8217d2b2943fb923090e15
0xsuda <138703534+0xsuda@users.noreply.github.com>
2023-10-16 21:59:25 +0300

    Update topparse.c

--- topparse.c
+++ topparse.c
@@ -1,12 +1,15 @@
 #include "top.h"
 char **Parse(char *input, const char *delim) {
+  char *tok;
+   int u;
+  int j;
   char *input_cp = strdup(input); 
   if (input_cp == NULL) {
     perror("Error allocating memory for input_cp:");
     return NULL;
   }
 
-  char *tok;
+  
   int tok_counter = 0;
   char **arr = NULL;
 
@@ -25,12 +28,12 @@ char **Parse(char *input, const char *delim) {
   }
 
   tok = strtok(input_cp, delim);
-  int u;
+ 
   for (u = 0; tok != NULL; u++) {
     arr[u] = strdup(tok); 
     if (arr[u] == NULL) {
       perror("Error allocating memory for arr[u]:");
-      for (int j = 0; j < u; j++) {
+      for ( j = 0; j < u; j++) {
         free(arr[j]);
       }
       free(arr);
