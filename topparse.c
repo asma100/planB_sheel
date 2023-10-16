be707fa373cacb4520366ceb3f3bbc7ba9bef0c8
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-16 14:27:01 +0300

    Update topparse.c

--- topparse.c
+++ topparse.c
@@ -8,6 +8,8 @@ char  **Parse(char * input, const char *delim)
  char **arr = NULL;
  int u = 0;
  int j;
+    checkline = getline(&input, &s, stdin);
+    
     input_cp = malloc(sizeof(char) * (checkline + 1));
  if (input_cp == NULL) {
   perror("Error allocating memory:");
