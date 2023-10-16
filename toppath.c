b766a378a0746865e8bb62c67931f8df5d0308de
0xsuda <138703534+0xsuda@users.noreply.github.com>
2023-10-17 02:42:21 +0300

    Update toppath.c

--- toppath.c
+++ toppath.c
@@ -1,24 +1,23 @@
 #include "top.h"
 char *finding_path(char *command) {
  char *thepath, *pc, *pt, *fp;
-/* int cmd_len, d_len;*/
+int cmd_len, d_len;
 
  struct stat s_buff;
  thepath = getenv("PATH");
  if (thepath) {
   pc = strdup(thepath);
- /* cmd_len = strlen(command);*/
+  cmd_len = strlen(command);
   pt = strtok(pc, ":");
   while (pt != NULL) {
-  /* d_len = strlen(pt);*/
-   fp = malloc(1024);
+   d_len = strlen(pt);
+   fp = malloc(cmd_len + d_len + 2);
    strcpy(fp, pt);
    strcat(fp, "/");
    strcat(fp, command);
-   /*strcat(fp, "\0");*/
+   strcat(fp, "\0");
    if (stat(fp, &s_buff) == 0) {
     free(pc);
-    free(fp);
     return (fp);
    } else {
     pt = strtok(NULL, ":");
