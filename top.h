de18cd8b2c116881c166ff25c4eca2c19c6b6843
0xsuda <138703534+0xsuda@users.noreply.github.com>
2023-10-16 21:55:54 +0300

    Update top.h

--- top.h
+++ top.h
@@ -13,7 +13,7 @@ void topcmd(char **argv);
 int _strlen(char *s);
 char  **Parse(char * input, const char *delim);
 char *finding_path(char *command);
-void inputtop (char *input, size_t s);
+void inputtop (char *input);
 /*void read_command(char *command, size_t size);
   void execute_command(const char *command);*/
 
