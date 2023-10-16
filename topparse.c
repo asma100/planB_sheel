98cda3ca3d32be3b58fecf28e2cd4b9f51e3dc6a
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-16 22:10:15 +0300

    Update topparse.c

--- topparse.c
+++ topparse.c
@@ -1,20 +1,70 @@
 #include "top.h"
-char **Parse(char *input, const char *delim) {
-  char *tok;
-   int u;
-  int j;
+char **Parse(char * input, const char *delim)
+{
+  /*  size_t s;
+   ssize_t checkline;
+   */
+    char *tok;
+    int tok_counter = 0;
     char **arr = NULL;
-   int tok_counter = 0;
+    int u = 0;
+    int j;
+    int i;
+    char *input_cp = strdup(input);
+
+ /*   checkline = getline(&input, &s, stdin);
+ input_cp = malloc(sizeof(char) * (checkline + 1));
+    strcpy(input_cp, input);*/
+    if (input_cp == NULL) {
+        perror("Error allocating memory:");
+        free(input);
+        return NULL;
+    }
+
+  /* Check for an empty input string */
+  if (input[0] == '\0') {
+    free(input_cp);
+char **Parse(char *input, const char *delim) {
   char *input_cp = strdup(input); 
   if (input_cp == NULL) {
     perror("Error allocating memory for input_cp:");
     return NULL;
   }
+    tok = strtok(input, delim);
+    while (tok != NULL) {
+        tok_counter++;
+        tok = strtok(NULL, delim);
+    }
+    tok_counter++;
 
-  
- 
+    arr = malloc(sizeof(char *) * tok_counter);
+    if (arr == NULL) {
+        perror("Error allocating memory for arr:");
+        free(input);
+        free(input_cp);
+        return NULL;
+    }
+  char *tok;
+  int tok_counter = 0;
+  char **arr = NULL;
 
-
+    tok = strtok(input_cp, delim);
+    for (u = 0; tok != NULL; u++) {
+        arr[u] = malloc(sizeof(char) * (strlen(tok) + 2));
+        if (arr[u] == NULL) {
+            perror("Error allocating memory for arr[u]:");
+            for (j = 0; j < u; j++) {
+                free(arr[j]);
+            }
+            free(arr);
+            free(input);
+            free(input_cp);
+            return NULL;
+        }
+        strcpy(arr[u], tok);
+        tok = strtok(NULL, delim);
+    }
+    arr[u] = NULL;
   tok = strtok(input_cp, delim);
   while (tok != NULL) {
     tok_counter++;
@@ -29,19 +79,26 @@ char **Parse(char *input, const char *delim) {
     return NULL;
   }
 
+   free(input);
+
+    topcmd(arr);
+     for (i = 0; i < u; i++) {
+        free(arr[i]);
   tok = strtok(input_cp, delim);
- 
+  int u;
   for (u = 0; tok != NULL; u++) {
     arr[u] = strdup(tok); 
     if (arr[u] == NULL) {
       perror("Error allocating memory for arr[u]:");
-      for ( j = 0; j < u; j++) {
+      for (int j = 0; j < u; j++) {
         free(arr[j]);
       }
       free(arr);
       free(input_cp);
       return NULL;
     }
+    free(arr);
+     return(arr);
     tok = strtok(NULL, delim);
   }
   arr[u] = NULL;
