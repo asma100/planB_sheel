99b16d124bdd0e1facec4adb7761ed71b7c5e746
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-16 14:56:05 +0300

    Update inputtop.c

--- inputtop.c
+++ inputtop.c
@@ -1,22 +1,23 @@
 #include "top.h" 
-void inputtop (char *input, size_t s){
- ssize_t checkline;
-checkline = getline(&input, &s, stdin);
- if (checkline == -1) {
-  if (errno == EOF) {
-   printf("The user has finished entering input.\n");
-  } else {
-  perror("Error reading input:");
-  free(input);
- }
- }
-  else if (checkline == 1 && input[0] == '\n')
- {
- /* empty input, do nothing*/
- print_top("top$");
- }
- else if (strcmp(input, "exit\n") == 0)
- {
+void inputtop(char *input, size_t s) {
+  ssize_t checkline;
 
- }
+  checkline = getline(&input, &s, stdin);
+  if (checkline == -1) {
+    if (errno == EOF) {
+      printf("The user has finished entering input.\n");
+    } else {
+      perror("Error reading input:");
+      free(input);
+      exit(EXIT_FAILURE);
+    }
+  } else if (checkline == 1 && input[0] == '\n')
+  {
+    /* empty input, do nothing*/
+    print_top("top$");
+  }
+  else if (strcmp(input, "exit\n") == 0) {
+    /* user wants to exit the program */
+    exit(EXIT_SUCCESS);
+  }
 }
