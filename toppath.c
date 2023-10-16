88f3808b67e29824f31b5eb327d0a6c393b1d7bb
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-16 22:17:21 +0300

    Update toppath.c

--- toppath.c
+++ toppath.c
@@ -2,18 +2,12 @@
 char *finding_path(char *command) {
   char *thepath, *pc, *pt, *fp;
   int cmd_len, d_len;
-
   struct stat s_buff;
   thepath = getenv("PATH");
   if (thepath) {
     pc = strdup(thepath);
     cmd_len = strlen(command);
     pt = strtok(pc, ":");
-    fp = NULL;  
-  if (!thepath) {
-    return NULL;  
-  }
-
     while (pt != NULL) {
       d_len = strlen(pt);
       fp = malloc(cmd_len + d_len + 2);
@@ -38,41 +32,8 @@ char *finding_path(char *command) {
     if (stat(command, &s_buff) == 0) {
       return (command);
     }
-  pc = strdup(thepath);
-  if (!pc) {
-    perror("Error allocating memory for pc:");
-    return NULL;
   }
-
-  /* Free the memory for the fp variable if it was allocated and not used to find the command. */
-  if (fp != NULL) {
-    free(fp);
-  cmd_len = strlen(command);
-  pt = strtok(pc, ":");
-  fp = NULL;  
-
-  while (pt != NULL) {
-    d_len = strlen(pt);
-    fp = malloc(cmd_len + d_len + 2);
-    if (!fp) {
-      perror("Error allocating memory for fp:");
-      free(pc);
-      return NULL;
-    }
-    strcpy(fp, pt);
-    strcat(fp, "/");
-    strcat(fp, command);
-    strcat(fp, "\0");
-    if (stat(fp, &s_buff) == 0) {
-      free(pc);
-      return fp;  
-    } else {
-      free(fp);
-      pt = strtok(NULL, ":");
-    }
-  }
+  /* Free the memory for the fp variable, even if we are not able to find the command.*/
+  free(fp);
   return (NULL);
-  free(pc);
-  free(fp);  
-  return NULL;
 }
