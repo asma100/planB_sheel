7a4aaeaf604f6e4127dee3cfac002ecc0414298c
0xsuda <138703534+0xsuda@users.noreply.github.com>
2023-10-16 21:51:22 +0300

    Update top.h

--- top.h
+++ top.h
@@ -1,21 +1,16 @@
 #ifndef TOP
 #define TOP
 #include <stddef.h>
-#include <sys/stat.h> 
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <errno.h>
+
 void print_top(char *top);
 void topcmd(char **argv);
 int _strlen(char *s);
-char  **Parse(char * input, const char *delim);
+char **Parse(char *input, const char *delim);
 char *finding_path(char *command);
-void inputtop (char *input, size_t s);
-/*void read_command(char *command, size_t size);
-  void execute_command(const char *command);*/
-
+char *inputtop(char *input);
 
 #endif
