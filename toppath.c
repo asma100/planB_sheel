cdc514081a9dbc63c504dfa0d18812922a8d3326
0xsuda <138703534+0xsuda@users.noreply.github.com>
2023-10-16 23:01:35 +0300

    Update toppath.c

--- toppath.c
+++ toppath.c
@@ -1,37 +1,55 @@
 #include "top.h"
-char *finding_path(char *command) {
- char *thepath, *pc, *pt, *fp;
- int cmd_len, d_len;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
- struct stat s_buff;
- thepath = getenv("PATH");
- if (thepath) {
-  pc = strdup(thepath);
-  cmd_len = strlen(command);
-  pt = strtok(pc, ":");
-  while (pt != NULL) {
-   d_len = strlen(pt);
-   fp = malloc(cmd_len + d_len + 2);
-   strcpy(fp, pt);
-   strcat(fp, "/");
-   strcat(fp, command);
-   strcat(fp, "\0");
-   if (stat(fp, &s_buff) == 0) {
-    free(pc);
-    return (fp);
-   } else {
-    free(fp);
-    pt = strtok(NULL, ":");
-   }
-  }
-  free(pc);
+char *finding_path(const char *command) {
+    const char *path = getenv("PATH");
+    if (path == NULL || command == NULL || command[0] == '\0') {
+        return NULL;
+    }
+
   
-  if (stat(command, &s_buff) == 0) {
-   return (command);
+    char *command_copy = strdup(command);
 
-  }
- free(fp);
- }
-  free(fp);
- return (NULL);
+    if (command_copy == NULL) {
+        return NULL;
+    }
+
+    char *token = strtok(command_copy, ":");
+    
+    while (token != NULL) {
+        struct stat s_buff;
+        size_t path_len = strlen(token);
+        size_t cmd_len = strlen(command);
+        char *full_path = (char *)malloc(path_len + cmd_len + 2); // +1 for '/' and +1 for '\0'
+
+        if (full_path == NULL) {
+            free(command_copy);
+            return NULL;
+        }
+
+        strcpy(full_path, token);
+        strcat(full_path, "/");
+        strcat(full_path, command);
+
+        if (stat(full_path, &s_buff) == 0 && S_ISREG(s_buff.st_mode) && (s_buff.st_mode & S_IXUSR)) {
+            free(command_copy);
+            return full_path;
+        }
+
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+
+    free(command_copy);
+
+ 
+    if (access(command, X_OK) == 0) {
+        return strdup(command);
+    }
+
+    return NULL;
 }
