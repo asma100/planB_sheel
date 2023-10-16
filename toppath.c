099d8e5ab7573f4f30a8974aec79428412338c14
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-16 22:09:08 +0300

    Update toppath.c

--- toppath.c
+++ toppath.c
@@ -7,20 +7,18 @@ char *finding_path(char *command) {
   thepath = getenv("PATH");
   if (thepath) {
     pc = strdup(thepath);
-    if (pc == NULL) {
-      perror("Error allocating memory for pc:");
-      return NULL;
-    }
-    
     cmd_len = strlen(command);
     pt = strtok(pc, ":");
-    fp = NULL;
+    fp = NULL;  
+  if (!thepath) {
+    return NULL;  
+  }
 
     while (pt != NULL) {
       d_len = strlen(pt);
-      fp = (char *)malloc(cmd_len + d_len + 2);
+      fp = malloc(cmd_len + d_len + 2);
       if (fp == NULL) {
-        perror("Error allocating memory for fp:");
+        perror("Error allocating memory:");
         free(pc);
         return NULL;
       }
@@ -30,19 +28,51 @@ char *finding_path(char *command) {
       strcat(fp, "\0");
       if (stat(fp, &s_buff) == 0) {
         free(pc);
-        return fp;
+        return (fp);
       } else {
         free(fp);
         pt = strtok(NULL, ":");
       }
     }
     free(pc);
-    free(fp);
-    
     if (stat(command, &s_buff) == 0) {
-      return strdup(command);
+      return (command);
     }
+  pc = strdup(thepath);
+  if (!pc) {
+    perror("Error allocating memory for pc:");
+    return NULL;
   }
 
+  /* Free the memory for the fp variable if it was allocated and not used to find the command. */
+  if (fp != NULL) {
+    free(fp);
+  cmd_len = strlen(command);
+  pt = strtok(pc, ":");
+  fp = NULL;  
+
+  while (pt != NULL) {
+    d_len = strlen(pt);
+    fp = malloc(cmd_len + d_len + 2);
+    if (!fp) {
+      perror("Error allocating memory for fp:");
+      free(pc);
+      return NULL;
+    }
+    strcpy(fp, pt);
+    strcat(fp, "/");
+    strcat(fp, command);
+    strcat(fp, "\0");
+    if (stat(fp, &s_buff) == 0) {
+      free(pc);
+      return fp;  
+    } else {
+      free(fp);
+      pt = strtok(NULL, ":");
+    }
+  }
+  return (NULL);
+  free(pc);
+  free(fp);  
   return NULL;
 }
