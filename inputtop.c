2f54e0bddb348669d11776107e20da95bc5dc4f5
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-16 11:35:21 +0300

    Create inputtop.c

---
+++ inputtop.c
@@ -0,0 +1,28 @@
+int inputtop (char *input, size_t s){
+
+checkline = getline(&input, &s, stdin);
+ if (checkline == -1) {
+  if (errno == EOF) {
+   printf("The user has finished entering input.\n");
+  } else {
+  perror("Error reading input:");
+  free(input);
+  free(input_cp);
+  free(argv);
+  return (-1);
+ }
+ }
+  else if (checkline == 1 && input[0] == '\n')
+ {
+ /* empty input, do nothing*/
+ continue;
+ }
+ else if (strcmp(input, "exit\n") == 0)
+ {
+ /* user entered "exit", quit program */
+ free(input);
+ free(input_cp);
+ free(argv); 
+ return (0);
+ }
+}
