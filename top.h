5669e30e388304f3375b453e544b327fe3bfc9f0
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-18 00:33:26 +0300

    Update top.h

--- top.h
+++ top.h
@@ -15,7 +15,7 @@ char *finding_path(char *command);
 char *inputtop (char *input, size_t s);
 void env_builtin();
 void print_error(const char *message);
-size_t _strlen(const char *str);
+size_t _strlen(const char *s);
 /*void read_command(char *command, size_t size);
   void execute_command(const char *command);*/
 
