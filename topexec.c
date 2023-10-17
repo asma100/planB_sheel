1176ccc15bb64fb4582311b1b07cb9c412647e77
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-17 13:35:53 +0300

    Update topexec.c

--- topexec.c
+++ topexec.c
@@ -37,7 +37,10 @@ void topcmd(char **argv){
     }
     free(acmd);    
           
-                    
+        for ( i = 0; argv[i] != NULL; i++) {
+                        free(argv[i]);
+                    }
+                        
              
                 
     acmd = NULL;
