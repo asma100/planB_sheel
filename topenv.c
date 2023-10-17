1149f7488664654bb0ece560a8bc6e75feafe2dc
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-18 02:26:27 +0300

    Update topenv.c

--- topenv.c
+++ topenv.c
@@ -1,8 +1,10 @@
 #include "top.h"
-void env_builtin() {
-   extern char **environ;
- char **env;
-    for (env = environ; *env != NULL; env++) {
-        printf("%s\n", *env);
-    }
+void env_builtin()
+{
+extern char **environ;
+char **env;
+for (env = environ; *env != NULL; env++)
+{
+printf("%s\n", *env);
+}
 }
