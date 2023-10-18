40830d09c40069fee1cd915269c6593240a241c3
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-18 09:59:01 +0300

    Update top.h

--- top.h
+++ top.h
@@ -10,13 +10,13 @@
 #include <errno.h>
 void print_top(char *top);
 void topcmd(char **argv);
-void Parse(char * input, const char *delim);
+void Parse(char *input, const char *delim);
 char *finding_path(char *command);
 char *inputtop (char *input, size_t s);
 void env_builtin();
 void print_error(const char *message);
 size_t _strlen(const char *s);
-void parsetok(char *input_cp, const char *delim);
+void parsetok(char *input_cp, char *input, const char *delim);
 /*void read_command(char *command, size_t size);
   void execute_command(const char *command);*/
 
