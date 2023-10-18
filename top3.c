f96baa939d06cb960197f14eee3adf6369e60126
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-18 17:20:03 +0300

    Update top3.c

--- top3.c
+++ top3.c
@@ -3,66 +3,39 @@
 #include <unistd.h>
 #include "top.h"
 
-int main(void)
-{  char *input = NULL;
-  /*  char **arr = NULL;
-  
-    const char *delim = " \t\n";*/
 
-    if (isatty(STDIN_FILENO))
-    {
-        /* shell is running in interactive mode */
-        while (1)
-        {
-            print_top("top$");
-           inputtop(input, sizeof(input));
-          
-        free(input);
-    }}
-    else
-    {
-        /* shell is running in non-interactive mode */
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t checkline;
 
-        checkline = getline(&line, &len, stdin);
-        if (checkline == -1)
-        {
-            if (feof(stdin))
-            {
-                printf("\n");
-                exit(EXIT_SUCCESS);
-            }
-            else
-            {
-                perror("Error reading input:");
-                free(line);
-                exit(EXIT_FAILURE);
-            }
-        }
+int main(void) {
+char **arr = NULL;
+ char *input = NULL;
+/*char *input_cp = NULL;*/
+ const char *delim = " \t\n";
+if (isatty (STDIN_FILENO)) {
+/* shell is running in interactive mode */
+/* display prompt and accept input from user */
+ while (1) {
+ print_top("top$");
+input= inputtop (input,sizeof(input));
+ Parse(input,delim);
 
-        while (checkline != -1)
-        {
-            /* read each line from standard input */
-            if (checkline == 1 && line[0] == '\n')
-            {
-                /* empty line, do nothing */
-                free(line);
-                continue;
-            }
-            else if (strcmp(line, "exit\n") == 0)
-            {
-                /* user wants to exit the program */
-                free(line);
-                line = NULL;
-                exit(EXIT_SUCCESS);
-            }
-            
 
-           
-        }
-    }
+ }
+ free(input);
+/* free(input_cp);*/
+} else {
+/* shell is running in non-interactive mode */
+/* execute commands from script or batch file*/
 
-    return (0);
+
+ char *line = NULL;
+ size_t len = 0;
+ ssize_t checkline;
+
+input= inputtop (input,sizeof(line));
+ Parse(line,delim);
+free(arr);
+free(line);
+
+
+ return (0);
 }
