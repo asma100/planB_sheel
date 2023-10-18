b91e89bece3fb20db3990c165e0c809376507be0
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-18 11:00:53 +0300

    Update inputtop.c

--- inputtop.c
+++ inputtop.c
@@ -1,35 +1,35 @@
 #include "top.h"
 char *inputtop(char *input, size_t s)
 {
-ssize_t checkline;
-checkline = getline(&input, &s, stdin);
-if (checkline == -1)
-{
-if (feof(stdin))
-{
-printf("\n");
-exit(EXIT_SUCCESS);
-}
-else
-{
-perror("Error reading input:");
-free(input);
-exit(EXIT_FAILURE);
-}
-}
-else if (checkline == 1 && input[0] == '\n')
-{
-/* empty input, do nothing*/
-free(input);
-input = NULL;
-return (NULL);
-}
-else if (strcmp(input, "exit\n") == 0)
-{
-/* user wants to exit the program */
-free(input);
-input = NULL;
-exit(EXIT_SUCCESS);
-}
-return (input);
+    ssize_t checkline;
+    checkline = getline(&input, &s, stdin);
+    if (checkline == -1)
+    {
+        if (feof(stdin))
+        {
+            printf("\n");
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            perror("Error reading input:");
+            free(input);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (checkline == 1 && input[0] == '\n')
+    {
+        /* empty input, return an empty string */
+        free(input);
+        input = strdup("");
+        return (input);
+    }
+    else if (strcmp(input, "exit\n") == 0)
+    {
+        /* user wants to exit the program */
+        free(input);
+        input = NULL;
+        exit(EXIT_SUCCESS);
+    }
+    return (input);
 }
