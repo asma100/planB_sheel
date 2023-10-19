aa32c6b66001f6fe7b3a653f68e45f63f4c7ad69
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 20:55:49 +0300

    Update inputtop.c

--- inputtop.c
+++ inputtop.c
@@ -29,12 +29,7 @@ void inputtop(char *input, size_t s, int status) {
 	else if (strcmp(input, "exit\n") == 0)
 	{
 		free(input);
-		if (status == 0)
-			exit(0);
-		else
-		{
-			write(STDERR_FILENO, exitstatus, strlen(exitstatus));
-			exit (2);
+	
 		}
 	}
 	else if (strcmp(input, "env\n") == 0)
