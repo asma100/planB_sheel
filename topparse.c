8d129e27cb1d7525d64c0286c758dd7bb64d290f
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 05:46:06 +0300

    Update topparse.c

--- topparse.c
+++ topparse.c
@@ -1,76 +1,68 @@
  #include "top.h"
 
-
-
-void empchack(char * input, char * input_cp){
-    if (input_cp == NULL) {
-        perror("Error allocating memory:");
-        free(input);
-        return;
-    }
-    
-  /* Check for an empty input string */
-  if (input[0] == '\0') {
-    free(input_cp);
-    return ;
-  }
-}
- void Parse(char * input, const char *delim)
+void Parse(char * input, const char *delim)
 {
- 
-    char *tok;
-    int tok_counter = 0;
-    char **arr = NULL;
-    int u = 0; 
-   int j;
-  int i;
-    char *input_cp = strdup(input);
+char *tok;
+int tok_counter = 0;
+char **arr = NULL;
+int i, j, u = 0; 
+char *input_cp = strdup(input);
 empchack(input, input_cp);
-
-    tok = strtok(input, delim);
-    while (tok != NULL) {
-        tok_counter++;
-        tok = strtok(NULL, delim);
-    }
-    tok_counter++;
-
-    arr = malloc(sizeof(char *) * tok_counter);
-    if (arr == NULL) {
-        perror("Error allocating memory for arr:");
-        free(input);
-        free(input_cp);
-        return ;
-    }
-
-    tok = strtok(input_cp, delim);
-    for (u = 0; tok != NULL; u++) {
-        arr[u] = malloc(sizeof(char) * (strlen(tok) + 2));
-        if (arr[u] == NULL) {
-            perror("Error allocating memory for arr[u]:");
-         for (j = 0; j < u; j++) {
-                free(arr[j]);
-            }
-            free(arr);
-            free(input);
-            free(input_cp);
-            return;
-        }
-        strcpy(arr[u], tok);
-        tok = strtok(NULL, delim);
-    }
-    arr[u] = NULL;
-
-  
-    topcmd(arr);
- 
-   for (i = 0; i < u; i++) {
-        free(arr[i]);
-    }  
-   
-
-    free(arr);
- 
-   free(input_cp);
-
+tok = strtok(input, delim);
+while (tok != NULL)
+{
+tok_counter++;
+tok = strtok(NULL, delim);
 }
- 
+tok_counter++;
+arr = malloc(sizeof(char *) * tok_counter);
+if (arr == NULL)
+{
+perror("Error allocating memory for arr:");
+free(input);
+free(input_cp);
+return ;
+}
+tok = strtok(input_cp, delim);
+for (u = 0; tok != NULL; u++)
+{
+arr[u] = malloc(sizeof(char) * (strlen(tok) + 2));
+if (arr[u] == NULL) 
+{
+perror("Error allocating memory for arr[u]:");
+for (j = 0; j < u; j++) 
+free(arr[j]);
+free(arr);
+free(input);
+free(input_cp);
+return;
+}
+strcpy(arr[u], tok);
+tok = strtok(NULL, delim);
+}
+arr[u] = NULL;
+topcmd(arr);
+void freep(arr,input_cp);
+}
+void freep(char **arr,char *input_cp) 
+{
+    for (i = 0; i < u; i++) 
+free(arr[i]);
+free(arr);
+free(input_cp)
+}
+
+void empchack(char * input, char * input_cp)
+{
+if (input_cp == NULL)
+{
+perror("Error allocating memory:");
+free(input);
+return;
+}
+if (input[0] == '\0')
+ {
+free(input_cp);
+return ;
+}
+}
