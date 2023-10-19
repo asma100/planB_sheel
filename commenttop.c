cf58101d734d12d0d384afb927700667f9c50e80
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 01:28:07 +0300

    Update commenttop.c

--- commenttop.c
+++ commenttop.c
@@ -3,7 +3,7 @@ char **comment(char **arr)
 {
 int i = 0;
 char **arrc = NULL;
-while(arr[i] != "#"||arr[i] != NULL )
+while(strcmp(a[i], "#") != 0  || arr[i] != NULL )
 {
 arrc[i]=arr[i];
 free(arr[i]);
