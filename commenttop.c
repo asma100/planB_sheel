f78e7a42299738ab3283472bb4482031b32de0c5
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 02:11:53 +0300

    Update commenttop.c

--- commenttop.c
+++ commenttop.c
@@ -1,12 +1,23 @@
-#include "top.h"
 char **comment(char **arr)
 {
     int i = 0;
     char **arrc = malloc(sizeof(char*) * (MAX_ARGS + 1)); 
-    while(arr[i] != NULL && strcmp(arr[i], "#") != 0)
+    while(arr[i] != NULL)
     {
+        int j = 0;
+        while(arr[i][j] != '\0')
+        {
+            if(arr[i][j] == '#')
+            {
+                arrc[i] = NULL;
+                goto next; // skip to next string
+            }
+            j++;
+        }
         arrc[i] = arr[i];
         i++;
+    next:
+        ;
     }
     arrc[i] = NULL; 
     return arrc; 
