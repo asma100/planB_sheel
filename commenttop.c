915d626c42b873ae05dfc38aa888244fabc827a0
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 15:39:31 +0300

    Update commenttop.c

--- commenttop.c
+++ commenttop.c
@@ -1,32 +1,36 @@
 #include "top.h"
 char **comment(char **arr, int tok_counter)
 {
-    int i = 0, count = 0;
-    char **ar = malloc(sizeof(char*) * (tok_counter + 1));
-    while(arr[i] != NULL)
+  int i = 0, j = 0, count = 0; int a = 0;
+  char **ar = malloc(sizeof(char*) * (tok_counter + 1));
+  while(arr[i] != NULL)
+  { 
+    while(arr[i][j] != '\0')
     {
-        int j = 0;
-        while(arr[i][j] != '\0')
-        {
-            if(arr[i][j] == '#')
-            {
-                break;
-            }
-            j++;
-        }
-        if(arr[i][j] != '#') 
-        {
-            ar[count] = strdup(arr[i]);
-            count++;
-        }
-        i++;
+      if(arr[i][j] == '#')
+      {
+        a = 1;
+        goto next;
+      }
+      j++;
     }
-    ar[count] = NULL;
-
-    for (i = 0; arr[i] != NULL; i++)
-        free(arr[i]);
-
-    free(arr);
-
-    return ar;
+    ar[count] = strdup(arr[i]);
+    count++;
+  
+  next:
+  if (a == 1)
+  {
+      break;
+  }
+   i++; 
+  }
+  ar[count] = NULL;
+   
+  for (i = 0; arr[i] != NULL; i++)
+    free(arr[i]);
+   
+  free(arr);
+  
+  
+  return ar; 
 }
