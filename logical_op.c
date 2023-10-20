462a7b2dc4e293302baab91d4ddb4a317f456024
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-21 02:26:48 +0300

    Update logical_op.c

--- logical_op.c
+++ logical_op.c
@@ -4,9 +4,24 @@
  * @argv: pointer to the command value
  * Return: void
  */
+void handle_logicalop(char **argv) {
+  int i = 0;
+  
+  int k = 0;
+  int count = 0;
+  char **new_argv;
 
-for (i = 0, i < count, i++) {
-  if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "") == 0) {
+  /*Count the number of arguments.*/
+  for (count = 0; argv[count] != NULL; count++) {
+    ;
+  }
+
+  /* Allocate space for the new argument vector.*/
+  new_argv = malloc(sizeof(char *) * (count + 1));
+
+  /*Copy new argument vector, skipping any logical operators.*/
+for (i = 0; i < count; i++) {
+  if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "||") == 0) {
     /* Execute the previous command */
     new_argv[k] = NULL; /* Add null pointer */
     if (execve(new_argv[0], new_argv, NULL) == -1) {
