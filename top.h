0cdacdb8a89c0d9c46468de4380b33229c2947d4
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 22:15:33 +0300

    Update top.h

--- top.h
+++ top.h
@@ -16,20 +16,20 @@
 
 char *comm(char *input);
 void free_exit(char *input);
-void empchack(char * input, char * input_cp);
-void freep(char **arrc,char *input_cp, int u);
+void empchack(char *input, char  *input_cp);
+void freep(char **arrc, char *input_cp, int u);
 extern char **environ;
-void handext(char *input,int status);
+void handext(char *input, int status);
 void handenv(char *input);
 void print_top(char *top);
 void inputtop (char *input, size_t s, int status);
-void env_builtin();
+void env_builtin(void);
 int empty(char *input);
 void print_top(char *top);
 void topcmd(char **argv);
 void Parse(char *input, const char *delim);
 char *finding_path(char *command);
-void env_builtin();
+void env_builtin(void);
 void print_error(const char *message);
 int _strlen(char *s);
 void parsetok(char *input_cp, char *input);
