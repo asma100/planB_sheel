3dc6bfeed68d5f6e371f360c2887aade6650d8df
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 23:45:04 +0300

    Update logical_op.c

--- logical_op.c
+++ logical_op.c
@@ -1,20 +1,21 @@
 #include "top.h"
-void handle_logicalop(char **argv, int tok_counter) {
+void handle_logicalop(char **argv) {
   int i = 0;
   int j = 0;
   int k = 0;
+  count = 0;
   char **new_argv;
 
   /*Count the number of arguments.*/
-  for (i = 0; argv[i] != NULL; i++) {
+  for (count = 0; argv[count] != NULL; count++) {
     ;
   }
 
   /* Allocate space for the new argument vector.*/
-  new_argv = malloc(sizeof(char *) * (i + 1));
+  new_argv = malloc(sizeof(char *) * (count + 1));
 
   /*Copy the arguments into the new argument vector, skipping any logical operators.*/
-  for (i = 0; i < tok_counter; i++) {
+  for (i = 0; i < count; i++) {
     if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "||") == 0) {
       continue;
     }
