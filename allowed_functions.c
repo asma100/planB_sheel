d54332370a0b9367f4c1d62f03093231e3d7b84a
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 11:15:21 +0300

    Update allowed_functions.c

--- allowed_functions.c
+++ allowed_functions.c
@@ -1,12 +1,12 @@
 #include "top.h"
-/*int _strlen(char *s) {
+int _strlen(char *s) {
  int cou = 0;
  while (*s != '\0') {
  s++;
  cou++;
  }
  return (cou);
-}*/
+}
 
 void print_error(const char *message) {
   size_t len = strlen(message);
