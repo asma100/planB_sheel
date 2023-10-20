28854339afc9c263c5812bf4603aea239f1d1540
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 23:25:29 +0300

    Update logical_op.c

--- logical_op.c
+++ logical_op.c
@@ -5,15 +5,15 @@ void handle_logicalop(char **argv) {
   int k = 0;
   char **new_argv;
 
-  // Count the number of arguments.
+  /*Count the number of arguments.*/
   for (i = 0; argv[i] != NULL; i++) {
     ;
   }
 
-  // Allocate space for the new argument vector.
+  /* Allocate space for the new argument vector.*/
   new_argv = malloc(sizeof(char *) * (i + 1));
 
-  // Copy the arguments into the new argument vector, skipping any logical operators.
+  /*Copy the arguments into the new argument vector, skipping any logical operators.*/
   for (i = 0; i < argc; i++) {
     if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "||") == 0) {
       continue;
@@ -22,10 +22,10 @@ void handle_logicalop(char **argv) {
     new_argv[k++] = strdup(argv[i]);
   }
 
-  // Terminate the argument vector with a NULL pointer.
+  /* Terminate the argument vector with a NULL pointer.*/
   new_argv[k] = NULL;
 
-  // Execute the command with the new argument vector.
+  /* Execute the command with the new argument vector.*/
   if (strcmp(argv[j], "&&") == 0) {
     if (execvp(new_argv[0], new_argv) == -1) {
       perror("execvp");
@@ -40,7 +40,7 @@ void handle_logicalop(char **argv) {
     }
   }
 
-  // Free the memory allocated for the new argument vector.
+  /* Free the memory allocated for the new argument vector.*/
   for (i = 0; i < k; i++) {
     free(new_argv[i]);
   }
