3c711bc413fde661356663d7c700b89482dedac6
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 11:33:07 +0300

    Update topexec.c

--- topexec.c
+++ topexec.c
@@ -1,50 +1,42 @@
-#include "top.h"
-void topcmd(char **arr)
+void topcmd(char **argv)
 {
-    char *cmd = NULL, *acmd = NULL;
-    pid_t pid;
-    int fd_in, fd_out, i;
-
-    if (arr)
-    {     
-        
-        cmd = arr[0];
-         
-        acmd = finding_path(cmd);
-        if (acmd == NULL)
-        {
-            perror("Error:");
-            return;
-        }
-
-       
-
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("Error:");
-        }
-        else if (pid == 0)
-        {
-            if (strcmp(cmd, "env") == 0)
-            {
-                env_builtin();
-            }
-            else if (execve(acmd, arr, NULL) == -1)
-            {
-                perror("Error:");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            int status;
-            if (waitpid(pid, &status, 0) == -1)
-            {
-                perror("Error:");
-            }
-        }
-    }
-    if (cmd != acmd)
-        free(acmd);
+char *cmd = NULL, *acmd = NULL;
+pid_t pid;
+if (argv)
+{
+cmd = argv[0];
+acmd = finding_path(cmd);
+if (acmd == NULL)
+{
+perror("Error:");
+return;
+}
+pid = fork();
+if (pid == -1)
+{
+perror("Error:");
+}
+else if (pid == 0)
+{
+if (strcmp(cmd, "env") == 0)
+{
+env_builtin();
+}
+else if (execve(acmd, argv, NULL) == -1)
+{
+perror("Error:");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+int status;
+if (waitpid(pid, &status, 0) == -1)
+{
+perror("Error:");
+}
+}
+}
+if (cmd != acmd)
+free(acmd);
 }
