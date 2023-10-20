8962c1da322c70213a3cca18cfd30baaf82b38ba
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 12:35:30 +0300

    Update commenttop.c

--- commenttop.c
+++ commenttop.c
@@ -1,40 +1,40 @@
-#include "top.h"
-char **comment(char **arr,int tok_counter)
+char **comment(char **arr, int tok_counter)
 {
-  int i = 0, j = 0,m = 0;
-  char **ar = malloc(sizeof(char*) * tok_counter);
-   char **arrc;
-  while(arr[i] != NULL)
-  {
-    j = 0;
-    while(arr[i][j] != '\0')
+    int i = 0, j = 0, m = 0;
+    char **ar = malloc(sizeof(char*) * tok_counter);
+    char **arrc;
+    while(arr[i] != NULL)
     {
-      if(arr[i][j] == '#')
-      {
-        ar[i] = NULL;
-        goto next;
-      }
-      j++;
+        j = 0;
+        while(arr[i][j] != '\0')
+        {
+            if(arr[i][j] == '#')
+            {
+                ar[i] = NULL;
+                goto next;
+            }
+            j++;
+        }
+        ar[i] = strdup(arr[i]);
+        i++;
+    next:
+        i++; 
     }
-     if (ar[i] != NULL) {
-      ar[i] = strdup(arr[i]);
-     }
-    i++;
-  next:
-    i++; 
-  }
-   arrc = malloc(sizeof(char*) * i);
-  while (m<i)
-    { if (ar[m] != NULL) 
+    arrc = malloc(sizeof(char*) * (i + 1));
+    m = 0;
+    while (m < i)
     {
-      arrc[m] = strdup(arr[m]); 
+        if (ar[m] != NULL) 
+        {
+            arrc[m] = strdup(ar[m]); 
+        }
+        m++;
     }
-    m++;
-    }
+    arrc[m] = NULL;
    
-  free(arr);
-  free(ar);
+    free(arr);
+    free(ar);
    
   
-  return arrc; 
+    return arrc; 
 }
