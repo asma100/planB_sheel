dcc68879008b9fd9ac5c9d7013461b40c4e6ca7e
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 21:12:01 +0300

    Update inputtop.c

--- inputtop.c
+++ inputtop.c
@@ -26,42 +26,40 @@ int empty(char *input)
  * Return: null                                                                                                                                                         
 */
 void inputtop(char *input, size_t s, int status) {
-	ssize_t checkline;
-	 const char *delim = " \t\n";
+  ssize_t checkline;
+  const char *delim = " \t\n";
 
-	checkline = getline(&input, &s, stdin);
-	if (checkline == -1)
-	{
-		free(input);
-		exit(EXIT_SUCCESS);
-	}
-	else if (strcmp(input, "exit\n") == 0)
-	{
-		free(input);
-		if (status == 0)
-			exit(0);
-		else
-		{
-			
-			exit (2);
-		}
-	}
-	else if (strcmp(input, "env\n") == 0)
-	{
-		env_builtin();
-		free(input);
-		exit (0);
-	}
-	else
-	{
-		if (empty(input) == 1)
-		{
-			free(input);
-			return;
-		}
+  checkline = getline(&input, &s, stdin);
+  if (checkline == -1) {
+    free(input);
+    exit(EXIT_SUCCESS);
+  } else if (strcmp(input, "exit\n") == 0) {
+    free(input);
+    if (status == 0)
+      exit(0);
+    else
+      exit(2);
+  } else if (strcmp(input, "env\n") == 0) {
+    env_builtin();
+    free(input);
+    exit(0);
+  } else {
+    if (empty(input) == 1) {
+      free(input);
+      return;
+    }
 
+    // Handle comments.
+    int i = 0;
+    while (input[i] != '\0') {
+      if (input[i] == '#') {
+        input[i] = '\0';
+        break;
+      }
+      i++;
+    }
 
-		Parse(input,delim);
-	}
-	free(input);
+    Parse(input, delim);
+  }
+  free(input);
 }
