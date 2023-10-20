df91797719637cea1563a99e81f6e52d5e071587
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-20 12:57:28 +0300

    Update commenttop.c

--- commenttop.c
+++ commenttop.c
@@ -1,15 +1,14 @@
-#include "top.h"
 char **comment(char **arr, int tok_counter)
 {
     int i = 0, j = 0, m = 0;
     char **ar = malloc(sizeof(char*) * tok_counter);
     char **arrc;
-    while(arr[i] != NULL)
+    while (arr[i] != NULL)
     {
         j = 0;
-        while(arr[i][j] != '\0')
+        while (arr[i][j] != '\0')
         {
-            if(arr[i][j] == '#')
+            if (arr[i][j] == '#')
             {
                 ar[i] = NULL;
                 goto next;
