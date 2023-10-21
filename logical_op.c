bd4b69309c123d4479c5b6ef5ef8f7a99c371940
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-21 03:21:03 +0300

    Update logical_op.c

--- logical_op.c
+++ logical_op.c
@@ -4,54 +4,39 @@
  * @argv: pointer to the command value
  * Return: void
  */
-#include "top.h"
-/**
- * handle_logicalop - function  handlel ogical op
- * @argv: pointer to the command value
- * Return: void
- */
 void handle_logicalop(char **argv) {
   int i = 0;
-  
   int k = 0;
   int count = 0;
   char **new_argv;
+
   /*Count the number of arguments.*/
   for (count = 0; argv[count] != NULL; count++) {
     ;
   }
+
   /* Allocate space for the new argument vector.*/
   new_argv = malloc(sizeof(char *) * (count + 1));
-  /*Copy new argument vector, skipping any logical operators.*/
-  for (i = 0; i < count; i++) {
-    if (strcmp(argv[i], "&&") == 0 || strcmp(argv[i], "||") == 0) {
-      /* Execute the previous command */
-      new_argv[k] = NULL; /* Add null pointer */
-      if (execve(new_argv[0], new_argv, NULL) == -1) {
-        perror("execve");
-        return;
-      }
 
-      /* Create a new argument vector for the next command */
-      new_argv = malloc(sizeof(char *) * (count + 1));
-      k = 0;
+  /* Loop through all commands separated by logical operators */
+  while (i < count) {
+    k = 0;
 
-      continue;
+    /* Copy new argument vector, skipping any logical operators */
+    while (argv[i] != NULL && strcmp(argv[i], "&&") != 0 && strcmp(argv[i], "") != 0) {
+      new_argv[k++] = strdup(argv[i++]);
     }
 
-  
-    new_argv[k++] = strdup(argv[i]);
+    /* Add null pointer to end of argument vector */
+    new_argv[k] = NULL;
+
+    /* Execute the command */
+    if (execvp(new_argv[0], new_argv) == -1) {
+      perror("execvp");
+      return;
+    }
+
+    /* Skip over logical operator */
+    i++;
   }
-  /* Add null pointer to end of last argument vector */
-  new_argv[k] = NULL;
-  /* Execute the last command */
-  if (execve(new_argv[0], new_argv, NULL) == -1) {
-    perror("execve");
-    return;
-  }
-  /* Free the memory allocated for the new argument vector.*/
-  for (i = 0; i < k; i++) {
-    free(new_argv[i]);
-  }
-  free(new_argv);
 }
